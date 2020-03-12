444baad820a64948dce07ded0d9e253ac544e787
rubenslj00 <rubenslj00@gmail.com>
2020-03-12 01:02:49 -0400

    ju

--- webapp/src/main/webapp/script.js
+++ webapp/src/main/webapp/script.js
@@ -4,24 +4,11 @@
 #Author: Ruben Louis jean
 
 #echo -e "\n Cleanup docker images"
-docker rmi -f $(docker images | grep -w "<none>" | awk {print $3})
+docker rmi  $(docker images) > /dev/null 2>&1
 
-# Do not run if removal already in progress.
-pgrep "docker rm" && exit 0
-
-#Delete docker images older then one month.
-docker rmi -f $(docker images | awk '{print $3,$4,$5}' | grep '[5-9]\{1\}\ weeks\|years\|months' | awk '{print $1}')
+#stop container
+docker container stop $(docker container ls -aq) > /dev/null 2>&1
 
 # Remove Dead and Exited containers.
-docker rm $(docker ps -a | grep "Dead\|Exited" | awk '{print $1}'); true
-
-# It will fail to remove images currently in use.
-docker rmi $(docker images -qf dangling=true); true
-
-# Clean up unused docker volumes
-docker volume rm $(docker volume ls -qf dangling=true); true
-
-#Delete or clean up unused docker volumes.
-docker rmi -f $(docker volume ls -qf dangling=true)
-~
+docker container rm $(docker container ls -aq) > /dev/null 2>&1
 
