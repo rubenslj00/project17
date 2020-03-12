7db3188bea66a8efdf9922912f4d2081a817203a
rubenslj00 <rubenslj00@gmail.com>
2020-03-11 23:39:54 -0400

    j

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-# Pull base image 
-From tomcat:8-jre8 
-
-# Maintainer 
-MAINTAINER "kserge2001@yahoo.fr" 
-COPY ./webapp.war /usr/local/tomcat/webapps
--- webapp/src/main/webapp/script.js
+++ webapp/src/main/webapp/script.js
@@ -1,100 +1,27 @@
-/*credits: 
-pure css laughing santa from: https://codepen.io/Alireza29675/pen/KwgwMy 
-pure css Tree with glowing orbs from:
-https://codepen.io/martinwolf/pen/DyrwK/
+#!/bin/bash
+#Description: clean my docker images
+#Date: 3/9/2020
+#Author: Ruben Louis jean
 
-snowing animation using JS from:
-https://codepen.io/jywahu/pen/RrGKvw
-*/
-var makeitSnow = function() {
-  //canvas init
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  //canvas dimensions
-  var W = $(".frame").innerWidth();
-  var H = $(".frame").innerHeight();
-  canvas.width = W;
-  canvas.height = H;
+#echo -e "\n Cleanup docker images"
+docker rmi -f $(docker images | grep -w "<none>" | awk {print $3})
 
-  //snowflake particles
-  var mp = 100; //max particles
-  var particles = [];
-  for (var i = 0; i < mp; i++) {
-    particles.push({
-      x: Math.random() * W, //x-coordinate
-      y: Math.random() * H, //y-coordinate
-      r: Math.random() * 4 + 1, //radius
-      d: Math.random() * mp //density
-    })
-  }
+# Do not run if removal already in progress.
+pgrep "docker rm" && exit 0
 
-  //Lets draw the flakes
-  function draw() {
-    ctx.clearRect(0, 0, W, H);
+#Delete docker images older then one month.
+docker rmi -f $(docker images | awk '{print $3,$4,$5}' | grep '[5-9]\{1\}\ weeks\|years\|months' | awk '{print $1}')
 
-    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-    ctx.beginPath();
-    for (var i = 0; i < mp; i++) {
-      var p = particles[i];
-      ctx.moveTo(p.x, p.y);
-      ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
-    }
-    ctx.fill();
-    update();
-  }
+# Remove Dead and Exited containers.
+docker rm $(docker ps -a | grep "Dead\|Exited" | awk '{print $1}'); true
 
-  //Function to move the snowflakes
-  //angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
-  var angle = 0;
+# It will fail to remove images currently in use.
+docker rmi $(docker images -qf dangling=true); true
 
-  function update() {
-    angle += 0.01;
-    for (var i = 0; i < mp; i++) {
-      var p = particles[i];
-      //Updating X and Y coordinates
-      //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
-      //Every particle has its own density which can be used to make the downward movement different for each flake
-      //Lets make it more random by adding in the radius
-      p.y += Math.cos(angle + p.d) + 1 + p.r / 2;
-      p.x += Math.sin(angle) * 2;
+# Clean up unused docker volumes
+docker volume rm $(docker volume ls -qf dangling=true); true
 
-      //Sending flakes back from the top when it exits
-      //Lets make it a bit more organic and let flakes enter from the left and right also.
-      if (p.x > W + 5 || p.x < -5 || p.y > H) {
-        if (i % 3 > 0) //66.67% of the flakes
-        {
-          particles[i] = {
-            x: Math.random() * W,
-            y: -10,
-            r: p.r,
-            d: p.d
-          };
-        } else {
-          //If the flake is exitting from the right
-          if (Math.sin(angle) > 0) {
-            //Enter from the left
-            particles[i] = {
-              x: -5,
-              y: Math.random() * H,
-              r: p.r,
-              d: p.d
-            };
-          } else {
-            //Enter from the right
-            particles[i] = {
-              x: W + 5,
-              y: Math.random() * H,
-              r: p.r,
-              d: p.d
-            };
-          }
-        }
-      }
-    }
-  }
+#Delete or clean up unused docker volumes.
+docker rmi -f $(docker volume ls -qf dangling=true)
+~
 
-  //animation loop
-  setInterval(draw, 33);
-}
-
-window.onload = makeitSnow();
